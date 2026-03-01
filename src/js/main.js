d41709987dffe368884c5267d1686fce41cee090
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-01 12:17:08 -0300

    Toggle show modifier on button click

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,6 @@
 const hamButton = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+
+hamButton.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu--show");
+});
