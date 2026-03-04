79f719d2c306b318d609b237d9ee06d28e56aa6f
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-04 18:19:34 -0300

    Toggle button image on click

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,6 @@ const hamButton = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamButton.addEventListener("click", () => {
+  hamButton.classList.toggle("hamburger--close");
   navMenu.classList.toggle("nav-menu--show");
 });
