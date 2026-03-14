95529716052962add1956372aaa0d3ff2a506c42
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-14 16:34:19 -0300

    Get saved theme from localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ hamButton.addEventListener("click", () => {
 const themeToggle = document.querySelector(".theme-toggle");
 
 themeToggle.addEventListener("click", () => {
+  const savedTheme = localStorage.getItem("theme");
   const currentTheme = document.documentElement.dataset.theme || "light";
   const theme = currentTheme === "dark" ? "light" : "dark";
   document.documentElement.setAttribute("data-theme", theme);
