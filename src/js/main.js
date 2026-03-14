8e1d9c436b19d977cc7acd33dfd17d5a12915c83
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-14 16:31:58 -0300

    Add listener to theme toggle

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,11 @@ hamButton.addEventListener("click", () => {
   hamButton.classList.toggle("hamburger--close");
   navMenu.classList.toggle("nav-menu--show");
 });
+
+const themeToggle = document.querySelector(".theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+  const currentTheme = document.documentElement.dataset.theme || "light";
+  const theme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", theme);
+});
