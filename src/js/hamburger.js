c65b9062e1f8e5f3c2aba23b634e9cf4825a667d
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-15 10:03:15 -0300

    Extract logic to toggle hamburger menu

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,9 +1,11 @@
 const hamButton = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+const toggleHamburgerMenu = () => {
+  hamButton.classList.toggle("hamburger--close");
+  navMenu.classList.toggle("nav-menu--show");
+};
+
 export const loadHamburger = () => {
-  hamButton.addEventListener("click", () => {
-    hamButton.classList.toggle("hamburger--close");
-    navMenu.classList.toggle("nav-menu--show");
-  });
+  hamButton.addEventListener("click", toggleHamburgerMenu);
 };
