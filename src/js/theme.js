95e7379f5f2f1529b20254466de46742ecd0fbd9
Gabriel <gabriel.gracilianomb@gmail.com>
2026-03-15 09:53:10 -0300

    Create updateToggler to change button state
    This function is executed on the first load of the page and when the theme is changed.

--- src/js/theme.js
+++ src/js/theme.js
@@ -12,19 +12,26 @@ const setTheme = (theme) => {
   localStorage.setItem("theme", theme);
 };
 
+const updateToggler = (theme) => {
+  const themeToggle = document.querySelector(".theme-toggle");
+  themeToggle.classList.toggle("theme-toggle--active", theme === "dark");
+};
+
 const changeTheme = () => {
   const currentTheme = getTheme();
   const theme = currentTheme === "dark" ? "light" : "dark";
   setTheme(theme);
+  updateToggler(theme);
 };
 
-const initThemeToggle = () => {
+const initThemeToggle = (theme) => {
   const themeToggle = document.querySelector(".theme-toggle");
   themeToggle.addEventListener("click", changeTheme);
+  updateToggler(theme);
 };
 
 export const loadAvailableTheme = () => {
   const theme = getTheme();
   setTheme(theme);
-  initThemeToggle();
+  initThemeToggle(theme);
 };
